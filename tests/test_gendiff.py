dd53244f86c5199f1cca93164dd63fa7e7c6cac3
SergeyAnuf <sergeysyava38@gmail.com>
2025-01-25 15:43:27 +0300

    исправлен тест

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,10 +5,8 @@ from gendiff.scripts.generate_diff import generate_dif
 
 def test_gendif():
     result = '- follow: False\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: True\n'
-    file1_path = 'tests/fixtures/file1.json'
-    file2_path = 'tests/fixtures/file2.json'
-    file1 = parser_file(file1_path)
-    file2 = parser_file(file2_path)
+    file1 = {"host": "hexlet.io", "timeout": "50", "proxy": "123.234.53.22", "follow": "False"}
+    file2 = {"timeout": "20", "verbose": "True", "host": "hexlet.io"}
     diff = generate_dif(file1, file2)
 
     assert diff == result
