de7852a763821ff6b1283a9db18c47b4438f5b37
SergeyAnuf <sergeysyava38@gmail.com>
2025-01-28 21:28:08 +0300

    test_gendiff

--- gendiff/scripts/file_parser.py
+++ gendiff/scripts/file_parser.py
@@ -9,7 +9,8 @@ def parser_file(file1_path, file2_path):
             if file1_path.endswith('json') and file2_path.endswith('json'):
                 file_1_dict = json.load(f1)
                 file_2_dict = json.load(f2)
-            elif file1_path.endswith('yaml' or 'yml') and file2_path.endswith('yaml' or 'yml'):
+            elif file1_path.endswith('yaml' or 'yml') 
+            and file2_path.endswith('yaml' or 'yml'):
                 file_1_dict = yaml.safe_load(f1)
                 file_2_dict = yaml.safe_load(f2)
         return file_1_dict, file_2_dict
