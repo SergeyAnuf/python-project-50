db7a08eae8ccdeed78452e73b96f3848eeac4448
SergeyAnuf <sergeysyava38@gmail.com>
2025-01-28 20:58:15 +0300

    add test_gendiff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -22,7 +22,7 @@ def test_gendif_json():
 def test_gendif_json():
     file1_path = 'tests/fixtures/file1.yaml'
     file2_path = 'tests/fixtures/file2.yaml'
-    result = ' - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n- timeout: 50\n'
+    result = ' - follow: False\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n'
     ' + timeout: 20\n + verbose: True\n'
     with open(file1_path) as f1, open(file2_path) as f2:
             file_1_dict = yaml.safe_load(f1)
