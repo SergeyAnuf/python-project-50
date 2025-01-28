7d0c181de48209db2ca2c182001063e26aa6c925
SergeyAnuf <sergeysyava38@gmail.com>
2025-01-28 20:53:15 +0300

    add test_gendiff

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -4,14 +4,14 @@ def generate_dif(file_1_dict, file_2_dict):
 
     for key, value in file_1_dict.items():
         if key not in file_2_dict:
-            diff += f'- {key}: {value}\n'
+            diff += f' - {key}: {value}\n'
         elif file_1_dict[key] == file_2_dict[key]:
-            diff += f'  {key}: {value}\n'
+            diff += f'   {key}: {value}\n'
         else:
-            diff += f'- {key}: {file_1_dict[key]}\n+ {key}: {file_2_dict[key]}\n'
+            diff += f' - {key}: {file_1_dict[key]}\n + {key}: {file_2_dict[key]}\n' 
 
     for key, value in file_2_dict.items():
         if key not in file_1_dict:
-            diff += f'+ {key}: {value}\n'
+            diff += f' + {key}: {value}\n'
 
     return diff
