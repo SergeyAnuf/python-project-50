be34f99dd871498bc8baa887393c1ef68cad7607
SergeyAnuf <sergeysyava38@gmail.com>
2025-01-28 21:44:44 +0300

    all

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -20,8 +20,7 @@ def test_gendif_json():
 def test_gendif_yaml():
     file1_path = 'tests/fixtures/file1.yaml'
     file2_path = 'tests/fixtures/file2.yaml'
-    result = '- follow: False\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n
- + verbose: True\n'
+    result = '- follow: False\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: True\n'
     with open(file1_path) as f1, open(file2_path) as f2:
         file_1_dict = yaml.safe_load(f1)
         file_2_dict = yaml.safe_load(f2)
