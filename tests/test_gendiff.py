54c63400e2f711de8f5c465c061a3b17a5a212a5
SergeyAnuf <sergeysyava38@gmail.com>
2025-01-28 20:14:46 +0300

    исправлены тесты

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,14 +3,28 @@ import json
 import yaml
 
 
-def test_gendif(file1_path, file2_path):
+def test_gendif_json():
+    file1_path = 'tests/fixtures/file1.json'
+    file2_path = 'tests/fixtures/file2.json'
     result = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n'
     '+ timeout: 20\n+ verbose: true\n'
     with open(file1_path) as f1, open(file2_path) as f2:
-        if file1_path.endswith('json') and file2_path.endswith('json'):
             file_1_dict = json.load(f1)
             file_2_dict = json.load(f2)
-        elif file1_path.endswith('yaml' or 'yml') and file2_path.endswith('yaml' or 'yml'):
+        
+            file_1_dict = yaml.safe_load(f1)
+            file_2_dict = yaml.safe_load(f2)
+    diff = generate_dif(file_1_dict, file_2_dict)
+
+    assert diff == result
+    
+    
+def test_gendif_json():
+    file1_path = 'tests/fixtures/file1.yaml'
+    file2_path = 'tests/fixtures/file2.yaml'
+    result = '- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n'
+    '+ timeout: 20\n+ verbose: true\n'
+    with open(file1_path) as f1, open(file2_path) as f2:
             file_1_dict = yaml.safe_load(f1)
             file_2_dict = yaml.safe_load(f2)
     diff = generate_dif(file_1_dict, file_2_dict)
