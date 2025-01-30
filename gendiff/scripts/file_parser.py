c048fb36c88ea3f1a65587e760a2d390cf303854
SergeyAnuf <sergeysyava38@gmail.com>
2025-01-30 11:08:25 +0300

    исправления

--- gendiff/scripts/file_parser.py
+++ gendiff/scripts/file_parser.py
@@ -6,7 +6,8 @@ import yaml
 def parser_file(file1_path, file2_path):
     try:
         with open(file1_path) as f1, open(file2_path) as f2:
-            if file1_path.endswith('json') and file2_path.endswith('json'):
+            if file1_path.endswith('json') 
+            and file2_path.endswith('json'):
                 file_1_dict = json.load(f1)
                 file_2_dict = json.load(f2)
             elif file1_path.endswith('yaml' or 'yml') 
