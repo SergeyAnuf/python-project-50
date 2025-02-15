df7090b4c2c8c47ea904d5cc3bf6dea1de8b79db
SergeyAnuf <sergeysyava38@gmail.com>
2025-02-15 16:33:20 +0300

    test_gendiff.py

---
+++ tests/test_gendiff.py
@@ -0,0 +1,25 @@
+
+from gendiff.formaters.stylish import format_diff
+from gendiff.scripts.file_parser import parser_file
+from gendiff.scripts.generate_diff1 import generate_dif
+from gendiff.formaters.formater_plain import process_changes
+
+    # Загружаем файлы
+file_1_dict, file_2_dict = parser_file(args.first_file, args.second_file)
+# Генерируем разницу
+diff = generate_dif(file_1_dict, file_2_dict)
+    
+    # Форматируем разницу
+if args.format == 'plain':
+    result = process_changes(diff)
+    file_result_path = 'tests/fixtures/file_result_plain.txt'
+    with open(file_result_path, 'r') as f5:
+        result_test = f5.read()
+
+else:
+    result = format_diff(diff)
+    file_result_path = 'tests/fixtures/file_result.txt'
+    with open(file_result_path, 'r') as f5:
+        result_test = f5.read()
+        
+assert result_test == result
