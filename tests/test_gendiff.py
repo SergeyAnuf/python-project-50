81e81c903319151218c81711128a4a6e34151737
SergeyAnuf <sergeysyava38@gmail.com>
2025-02-20 18:53:21 +0300

    test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,8 +7,8 @@ from gendiff.formaters.formater_plain import process_changes
 from gendiff.formaters.formater_json import transform_diff
 from gendiff.scripts.gendiff import main
 
-file1_path = 'tests/fixtures/file3.json'
-file2_path = 'tests/fixtures/file4.json'
+file1_path = 'file3.json'
+file2_path = 'file4.json'
 with open(file1_path, 'r') as f1, open(file2_path, 'r') as f2:
     file1 = f1.read()
     file2 = f2.read()
@@ -19,19 +19,19 @@ diff = generate_dif(file_1_dict, file_2_dict)
     # Форматируем разницу
 if args.format == 'plain':
     result = process_changes(diff)
-    file_result_path = 'tests/fixtures/file_result_plain.txt'
+    file_result_path = 'file_result_plain.txt'
     with open(file_result_path, 'r') as f5:
         result_test = f5.read()
         
 elif args.format == 'json':
     result = transform_diff(diff)
-    file_result_path = 'tests/fixtures/result_test_json.txt'
+    file_result_path = 'result_test_json.txt'
     with open(file_result_path, 'r') as f:
         result_test = f.read()
 
 else:
     result = format_diff(diff)
-    file_result_path = 'tests/fixtures/file_result.txt'
+    file_result_path = 'file_result.txt'
     with open(file_result_path, 'r') as f5:
         result_test = f5.read()
         
