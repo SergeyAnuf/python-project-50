aec78700c4b56cb10432840ef95be9d606672054
SergeyAnuf <sergeysyava38@gmail.com>
2025-02-21 17:59:00 +0300

    test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,61 +5,11 @@ from gendiff.formaters.formater_plain import process_changes
 from gendiff.formaters.formater_json import transform_diff
 
 def test_gendiff():
-    file_1_dict = {
-      "common": {
-        "setting1": "Value 1",
-        "setting2": 200,
-        "setting3": True,
-        "setting6": {
-          "key": "value",
-          "doge": {
-            "wow": ""
-          }
-        }
-      },
-      "group1": {
-        "baz": "bas",
-        "foo": "bar",
-        "nest": {
-          "key": "value"
-        }
-      },
-      "group2": {
-        "abc": 12345,
-        "deep": {
-          "id": 45
-        }
-      }
-    }
-
-    file_2_dict = {
-      "common": {
-        "setting1": "Value 1",
-        "setting2": 200,
-        "setting3": True,
-        "setting6": {
-          "key": "value",
-          "doge": {
-            "wow": ""
-          }
-        }
-      },
-      "group1": {
-        "baz": "bas",
-        "foo": "bar",
-        "nest": {
-          "key": "value"
-        }
-      },
-      "group2": {
-        "abc": 12345,
-        "deep": {
-          "id": 45
-        }
-      }
-    }
-    
-
+    file1_path = 'tests/fixtures/file3.json'
+    file2_path = 'tests/fixtures/file4.json'
+    with open(file1_path) as f1, open(file2_path) as f2:
+        file_1_dict = json.load(f1)
+        file_2_dict = json.load(f2)
 
     # Generate diff
     diff = generate_dif(file_1_dict, file_2_dict)
