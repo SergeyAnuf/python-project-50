47530ccd3131313e832877ce7e35aa97167781ba
Parsyan Lyova <l.parsyan@blask.com>
2025-03-02 17:47:51 +0400

    fix: ensure generate_diff correctly parses input files before processing

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -1,27 +1,20 @@
-def generate_diff(file_1_dict, file_2_dict):
+from gendiff.scripts.file_parser import parser_file
+
+
+def generate_diff(file1_path, file2_path):
+    file_1_dict, file_2_dict = parser_file(file1_path, file2_path)  # Загружаем файлы
     diff = {}
-    for key, value in file_2_dict.items():
-        if key in file_1_dict:
-            if isinstance(value, dict):
-                children1 = file_1_dict[key]
-                children2 = file_2_dict[key]
-                diff[key] = {
-                    "action": "nested",
-                    "children": generate_diff(children1, children2),
-                }
-            else:
-                if file_2_dict[key] == file_1_dict[key]:
-                    diff[key] = {"action": "unchange", "value": value}
-                elif file_2_dict[key] != file_1_dict[key]:
-                    diff[key] = {
-                        "action": "change",
-                        "old_value": file_1_dict[key],
-                        "new_value": file_2_dict[key],
-                    }
-        elif key not in file_1_dict:
-            diff[key] = {"action": "add", "value": value}
-    for key, value in file_1_dict.items():
+
+    for key, value in file_2_dict.items():  # Теперь file_2_dict точно словарь
+        if key not in file_1_dict:
+            diff[key] = {"status": "added", "value": value}
+        elif file_1_dict[key] != value:
+            diff[key] = {"status": "changed", "old_value": file_1_dict[key], "new_value": value}
+        else:
+            diff[key] = {"status": "unchanged", "value": value}
+
+    for key in file_1_dict:
         if key not in file_2_dict:
-            diff[key] = {"action": "remove", "value": value}
+            diff[key] = {"status": "removed", "value": file_1_dict[key]}
 
     return diff
