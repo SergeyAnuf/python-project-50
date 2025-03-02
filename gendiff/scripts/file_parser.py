ee271509ddaff454742c4495c4691872221665ff
Parsyan Lyova <l.parsyan@blask.com>
2025-03-02 17:44:34 +0400

    fix: correct generate_diff call in test to use file paths

--- gendiff/scripts/file_parser.py
+++ gendiff/scripts/file_parser.py
@@ -1,26 +1,16 @@
-import json
-
 import yaml
+import json
 
 
 def load_file(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as f:
-            if file_path.endswith('.json'):
-                return json.load(f)
-            elif file_path.endswith(('.yaml', '.yml')):
-                return yaml.safe_load(f)
-            else:
-                raise ValueError(f"Неподдерживаемый формат файла: {file_path}")
-    except FileNotFoundError:
-        print(f"Ошибка: Файл {file_path} не найден.")
-        return {}
-    except (json.JSONDecodeError, yaml.YAMLError):
-        print(f"Ошибка: Файл {file_path} содержит некорректные данные.")
-        return {}
+    with open(file_path, 'r', encoding='utf-8') as f:
+        if file_path.endswith('.json'):
+            return json.load(f)
+        elif file_path.endswith('.yml') or file_path.endswith('.yaml'):
+            return yaml.safe_load(f)
+        else:
+            raise ValueError("Unsupported file format")
 
 
 def parser_file(file1_path, file2_path):
-    file_1_dict = load_file(file1_path)
-    file_2_dict = load_file(file2_path)
-    return file_1_dict, file_2_dict
+    return load_file(file1_path), load_file(file2_path)
